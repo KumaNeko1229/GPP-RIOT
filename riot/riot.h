0346112f30654018c0770eca5bd94aca9a080ba5
Wei Le Jabier Ho <s10187854@connect.np.edu.sg>
2019-11-25 14:02:03 +0800

    fixed missing files

---
+++ riot/riot.h
@@ -0,0 +1,36 @@
+
+#ifndef _RIOT_H             // prevent multiple definitions if this 
+#define _RIOT_H            // ..file is included in more than one place
+#define WIN32_LEAN_AND_MEAN
+
+#include "game.h"
+#include "textureManager.h"
+#include "image.h"
+
+//=============================================================================
+// Create game class
+//=============================================================================
+class Riot : public Game
+{
+private:
+	// variables
+
+
+public:
+	// Constructor
+	Riot();
+
+	// Destructor
+	virtual ~Riot();
+
+	// Initialize the game
+	void initialize(HWND hwnd);
+	void update();      // must override pure virtual from Game
+	void ai();          // "
+	void collisions();  // "
+	void render();      // "
+	void releaseAll();
+	void resetAll();
+};
+
+#endif
