e9d1227affd2f54381b820de594d0c976e7a007d
Joel Tio <joeltiojinhon@gmail.com>
2019-11-26 23:54:43 +0800

    Replace entity id type with typedef

--- riot/entity.h
+++ riot/entity.h
@@ -4,18 +4,20 @@
 
 namespace ECS {
 
-template <typename EntityType>
+typedef unsigned int EntityIdType;
+
+template<typename EntityType>
 class Entity {
 	private:
-		int id;
+		EntityIdType id;
 		const std::type_info& entityType = typeid(EntityType);
 	public:
 		Entity() {};
-		Entity(int id) { this->id = id; };
+		Entity(EntityIdType id) { this->id = id; };
 		~Entity() {};
 
-		int getId() { return id; };
-		template <typename T> bool isType() { typeid(T) == entityType; };
+		EntityIdType getId() { return id; };
+		template<typename T> bool isType() { typeid(T) == entityType; };
 };
 
 }
