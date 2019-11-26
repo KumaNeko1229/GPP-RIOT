1cc353e269cdd8f575b7f368f14820bf3e2daa68
Joel Tio <joeltiojinhon@gmail.com>
2019-11-26 23:50:07 +0800

    Add entity

--- riot/entity.h
+++ riot/entity.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <typeinfo>
+
+namespace ECS {
+
+template <typename EntityType>
+class Entity {
+	private:
+		int id;
+		const std::type_info& entityType = typeid(EntityType);
+	public:
+		Entity() {};
+		Entity(int id) { this->id = id; };
+		~Entity() {};
+
+		int getId() { return id; };
+		template <typename T> bool isType() { typeid(T) == entityType; };
+};
+
+}
