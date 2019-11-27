9d08a8bdcec60c5655fafa320150b9d8e23f471d
Joel Tio <joeltiojinhon@gmail.com>
2019-11-27 11:19:04 +0800

    Fix system functions are not public

--- riot/system.h
+++ riot/system.h
@@ -3,18 +3,19 @@
 namespace ECS {
 
 class System {
-	System() {};
-	~System() {};
+	public:
+		System() {};
+		~System() {};
 
-	virtual void initialize() = 0;
+		virtual void initialize() = 0;
 
-	virtual void update() = 0;
+		virtual void update() = 0;
 
-	virtual void render() = 0;
+		virtual void render() = 0;
 
-	virtual void releaseAll() = 0;
+		virtual void releaseAll() = 0;
 
-	virtual void resetAll() = 0;
+		virtual void resetAll() = 0;
 };
 
 }
