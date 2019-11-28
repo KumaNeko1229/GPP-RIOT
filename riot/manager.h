d036601fb42615cd83125f5c8aab36d63d911fcf
Joel Tio <joeltiojinhon@gmail.com>
2019-11-28 15:01:16 +0800

    Move manager template implementation to tpp file Update manager code to use heap allocation

--- riot/manager.h
+++ riot/manager.h
@@ -8,18 +8,19 @@
 
 #include "typeUtil.h"
 #include "entity.h"
+#include "component.h"
 #include "system.h"
 
 namespace ECS {
 
 class Manager {
 	private:
-		// EntityId -> Entity
+		// EntityId -> Entity*
 		std::unordered_map<EntityIdType, Entity*> entities;
-		// EntityType -> unordered_set<EntityId>
+		// EntityType -> unordered_set<EntityId>*
 		std::unordered_map<Types::TypeId, std::unordered_set<EntityIdType>*> entityFamilies;
-		// Component -> vector<T>
-		std::unordered_map<Types::TypeId, void*> components;
+		// Component -> vector<Component>*
+		std::unordered_map<Types::TypeId, std::vector<Component>*> components;
 		// EntityId -> ComponentType -> vector index
 		std::unordered_map<EntityIdType, std::unordered_map<Types::TypeId, int>> entityComponents;
 		EntityIdType lastCreatedEntityId = NULL;
@@ -28,7 +29,7 @@ class Manager {
 	public:
 		template<typename EntityType> EntityIdType createEntity();
 
-		template<typename ComponentType> std::vector<ComponentType> getComponents();
+		template<typename ComponentType> std::vector<ComponentType>* getComponents();
 		template<typename ComponentType> ComponentType getEntityComponent(EntityIdType id);
 		std::vector<System*> getSystems() { return this->systems; };
 
@@ -39,3 +40,5 @@ class Manager {
 };
 
 }
+
+#include "manager.tpp"
