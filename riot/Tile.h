a3130768702ca78129c701478153b115c75639fd
KumaNeko <liu_wai_ho@yahoo.com.sg>
2019-12-03 16:01:45 +0800

    half way thru tile.cpp

---
+++ riot/Tile.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "entity.h"
+#include "Manager.h"
+#include "graphics.h"
+#include "constants.h"
+#include "position.h"
+#include "texture.h"
+#include "transform.h"
+
+namespace Entity {
+
+	class Tile : ECS::Entity {};
+
+	ECS::EntityIdType createTileEntity(ECS::Manager* manager, Graphics* graphics, int X, int Y, int TileType);
+
+}
