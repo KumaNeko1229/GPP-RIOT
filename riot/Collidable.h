fc5a89a756dfd008fa5d7d1ef7122cba0cd7bc95
Joel Tio <joeltiojinhon@gmail.com>
2019-12-05 23:43:14 +0800

    Add default empty function for collidable

--- riot/Collidable.h
+++ riot/Collidable.h
@@ -19,11 +19,14 @@ namespace Component {
 
 		std::unordered_set<ECS::EntityIdType> collidingWith;
 
-		std::function<void(ECS::Manager* manager, ECS::EntityIdType entityId)> onEnter;
+		std::function<void(ECS::Manager* manager, ECS::EntityIdType entityId)> onEnter =
+			[](ECS::Manager* manager, ECS::EntityIdType entityId) {};
 
-		std::function<void(ECS::Manager* manager, ECS::EntityIdType entityId, float frameTime)> onStay;
+		std::function<void(ECS::Manager* manager, ECS::EntityIdType entityId, float frameTime)> onStay =
+			[](ECS::Manager* manager, ECS::EntityIdType entityId, float frameTime) {};
 
-		std::function<void(ECS::Manager* manager, ECS::EntityIdType entityId)> onExit;
+		std::function<void(ECS::Manager* manager, ECS::EntityIdType entityId)> onExit =
+			[](ECS::Manager* manager, ECS::EntityIdType entityId) {};
 
 		D3DXVECTOR2 getCenter();
 		// Sorts vectors in order of: top left, top right, bottom left, bottom right
