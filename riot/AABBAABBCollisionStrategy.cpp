dce7a78de80742d5eae4996f350c091e6c2453e8
Joel Tio <joeltiojinhon@gmail.com>
2019-12-06 14:17:58 +0800

    Fix aabbaabb collision strategy and collidable sorted corners

--- riot/AABBAABBCollisionStrategy.cpp
+++ riot/AABBAABBCollisionStrategy.cpp
@@ -12,11 +12,13 @@ namespace CollisionUtil {
 	}
 
 	bool AABBAABBCollisionStrategy::isColliding(Component::Collidable source, Component::Collidable target) {
+		std::vector<D3DXVECTOR2> sourceCorners = source.getSortedCorners();
+		std::vector<D3DXVECTOR2> targetCorners = target.getSortedCorners();
 		return !(
 			// source on the left or right of target
-			source.corners[1].x < target.corners[0].x || target.corners[1].x < source.corners[0].x ||
+			sourceCorners[1].x < targetCorners[0].x || targetCorners[1].x < sourceCorners[0].x ||
 			// source above or below target
-			source.corners[0].y < target.corners[2].y || target.corners[0].y < source.corners[2].y
+			sourceCorners[2].y < targetCorners[0].y || targetCorners[2].y < sourceCorners[0].y
 		);
 	}
 
